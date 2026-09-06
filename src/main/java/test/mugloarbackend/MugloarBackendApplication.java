8c7e4f9af9824206b49846e865ea7aa11ed62c0f
ulvinasibli <nasibulvi@gmail.com>
2026-09-06 12:05:57 +0300

    first commit

---
+++ src/main/java/test/mugloarbackend/MugloarBackendApplication.java
@@ -0,0 +1,13 @@
+package test.mugloarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugloarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugloarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/mugloarbackend/MugloarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package test.mugloarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugloarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
