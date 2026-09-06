7b1e07ef0a4c0ce5b6fdc0b7fec365f1eb5e82aa
ulvinasibli <nasibulvi@gmail.com>
2026-09-06 12:07:48 +0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/mugloar/solver/MugloarSolverApplication.java
@@ -0,0 +1,11 @@
+package com.mugloar.solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugloarSolverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MugloarSolverApplication.class, args);
+	}
+}
