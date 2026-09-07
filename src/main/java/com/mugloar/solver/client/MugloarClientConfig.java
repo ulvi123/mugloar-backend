e60edfd5831c2e42680aeee4db3cbd3787388c89
ulvinasibli <nasibulvi@gmail.com>
2026-09-07 23:38:01 +0300

    Configure pooled HTTP client to avoid connection exhaustion under concurrent load

--- src/main/java/com/mugloar/solver/client/MugloarClientConfig.java
+++ src/main/java/com/mugloar/solver/client/MugloarClientConfig.java
@@ -1,23 +1,33 @@
 package com.mugloar.solver.client;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Configuration
 public class MugloarClientConfig {
 
 	@Bean
 	public RestTemplate mugloarRestTemplate(
-			RestTemplateBuilder builder,
-			@org.springframework.beans.factory.annotation.Value("${mugloar.api.connect-timeout-ms}") long connectTimeoutMs,
-			@org.springframework.beans.factory.annotation.Value("${mugloar.api.read-timeout-ms}") long readTimeoutMs) {
-		return builder
-				.setConnectTimeout(Duration.ofMillis(connectTimeoutMs))
-				.setReadTimeout(Duration.ofMillis(readTimeoutMs))
+			@Value("${mugloar.api.connect-timeout-ms}") int connectTimeoutMs,
+			@Value("${mugloar.api.read-timeout-ms}") int readTimeoutMs) {
+
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+		connectionManager.setMaxTotal(50);
+		connectionManager.setDefaultMaxPerRoute(30);
+
+		var httpClient = HttpClients.custom()
+				.setConnectionManager(connectionManager)
 				.build();
+
+		var requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+		requestFactory.setConnectTimeout(connectTimeoutMs);
+		requestFactory.setConnectionRequestTimeout(readTimeoutMs);
+
+		return new RestTemplate(requestFactory);
 	}
 }
